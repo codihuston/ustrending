0e61005f64c2c1e0fa3b94e553952d21abc5a1a9
Codi Huston (Home) <codi.huston540@gmail.com>
2020-10-16 12:26:38 -0500

    build: remove modules from root

--- worker-realtime/index.js
+++
@@ -1,9 +0,0 @@
-const googleTrends = require('google-trends-api');
-
-googleTrends.realTimeTrends({ geo: "US" }, function(err, results){
-  if(err){
-    console.error(error);
-  }else{
-    console.log(results);
-  }
-});
