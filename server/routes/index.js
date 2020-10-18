be05b4454fbcbd7bac080cd8134291e7d60d18f4
Codi Huston (Home) <codi.huston540@gmail.com>
2020-10-17 19:34:23 -0500

    todo(server): fetch daily/realtime trends from redis

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   return res.json({ title: 'Express' });
 });
 
+/**
+ * TODO: implement me
+ */
+router.get('/daily-trends', function(req, res, next) {
+  return res.json({ title: 'return daily trends from redis' });
+});
+
+/**
+ * TODO: implement me
+ */
+router.get('/realtime-trends', function(req, res, next) {
+  return res.json({ title: 'return realtime trends from redis' });
+});
+
 module.exports = router;
