9ae514596a71ce706d262910b2deaebfed5ddce9
Codi Huston (Home) <codi.huston540@gmail.com>
2020-10-18 05:27:14 -0500

    refactor: compontenize HomePage

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -20,7 +20,8 @@ import MapIcon from "@material-ui/icons/Map";
 import TableChartIcon from "@material-ui/icons/TableChart";
 import InfoIcon from "@material-ui/icons/Info";
 
-import MapChart from "./MapChart";
+// import MapChart from "./MapChart";
+import HomePage from "../pages/Home";
 import ListItemLink from "./ListItemLink";
 
 const drawerWidth = 240;
@@ -63,7 +64,6 @@ function ResponsiveDrawer(props) {
   const classes = useStyles();
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [content, setContent] = useState("");
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -144,48 +144,10 @@ function ResponsiveDrawer(props) {
         </nav>
         <main className={classes.content}>
           <div className={classes.toolbar} />
-          TODO: Show top trends marquee here?
           <Switch>
             <Route path="/chart">TODO: show chart</Route>
             <Route path="/about">TODO: show about</Route>
-            <Route path="/">
-              <Typography paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Rhoncus dolor purus non enim praesent elementum facilisis leo
-                vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-                hendrerit gravida rutrum quisque non tellus. Convallis convallis
-                tellus id interdum velit laoreet id donec ultrices. Odio morbi
-                quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod
-                quis viverra nibh cras. Metus vulputate eu scelerisque felis
-                imperdiet proin fermentum leo. Mauris commodo quis imperdiet
-                massa tincidunt. Cras tincidunt lobortis feugiat vivamus at
-                augue. At augue eget arcu dictum varius duis at consectetur
-                lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                sapien faucibus et molestie ac.
-              </Typography>
-              <Typography paragraph>
-                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-                ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-                elementum integer enim neque volutpat ac tincidunt. Ornare
-                suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-                volutpat consequat mauris. Elementum eu facilisis sed odio
-                morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                tincidunt ornare massa eget egestas purus viverra accumsan in.
-                In hendrerit gravida rutrum quisque non tellus orci ac.
-                Pellentesque nec nam aliquam sem et tortor. Habitant morbi
-                tristique senectus et. Adipiscing elit duis tristique
-                sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis.
-                Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              </Typography>
-              <MapChart setTooltipContent={setContent} />
-              <ReactTooltip html={true} multiline={true}>
-                {content}
-              </ReactTooltip>
-              TODO: Show trends in order per state here
-            </Route>
+            <Route path="/" component={HomePage}></Route>
           </Switch>
         </main>
       </Router>
