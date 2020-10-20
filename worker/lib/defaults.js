481056a7032aae5c29cd52ed6720fbf32b480772
Codi Huston (Home) <codi.huston540@gmail.com>
2020-10-20 11:41:26 -0500

    build(worker): raise max trending limit defaults
    - only done in defaults, NOT k8s

--- worker/lib/defaults.js
+++ worker/lib/defaults.js
@@ -1,5 +1,5 @@
 // for rate limiting google trends api calls
-module.exports.MAX_TREND_LIMIT = 0;
+module.exports.MAX_TREND_LIMIT = 5;
 // for google trends api config
 module.exports.GOOGLE_GEO_COUNTRY_CODE = "US";
 module.exports.GOOGLE_HOST_LANGUAGE = "en-US";
@@ -10,7 +10,7 @@ module.exports.GOOGLE_GEO_TIME_RANGES = "now 7-d";
 module.exports.CRON_EXPRESSION_DAILY_TRENDS = "0 * * * *";
 module.exports.CRON_EXPRESSION_REALTIME_TRENDS = "0 * * * *";
 // IANA time zone identifier
-module.exports.CRON_TIMEZONE = "America/Chicago"
+module.exports.CRON_TIMEZONE = "America/Chicago";
 // keys for redis
 module.exports.REDIS_DAILY_TRENDS_KEY = "daily";
 module.exports.REDIS_DAILY_TRENDS_BY_STATE_KEY = "daily-state";
