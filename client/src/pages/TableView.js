2180b68bcd2e65d4e37e8a8a16688b9224561932
Codi Huston (Home) <codi.huston540@gmail.com>
2020-10-28 16:28:43 -0500

    style(client): verbiage / header for Table

--- client/src/pages/TableView.js
+++ client/src/pages/TableView.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 import TrendingByStateTable from "../components/TrendingByStateTable";
 
 function TableView(props) {
-  return <TrendingByStateTable {...props}></TrendingByStateTable>;
+  return (
+    <Paper>
+      <Box p={3}>
+        <Typography variant="h2">Trending Table</Typography>
+        <Typography paragraph>
+          Below is a table-view of the trending data for each U.S. State.
+        </Typography>
+        <TrendingByStateTable {...props}></TrendingByStateTable>
+      </Box>
+    </Paper>
+  );
 }
 
 export default TableView;
