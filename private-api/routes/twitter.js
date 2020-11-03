661d77fe2c753ac47f2d5b5cd7896c82b167a399
Codi Huston (Home) <codi.huston540@gmail.com>
2020-11-03 16:22:15 -0600

    remove(private-api): unused endpoint

--- private-api/routes/twitter.js
+++ private-api/routes/twitter.js
@@ -44,29 +44,5 @@ router.post("/places/update", async function (req, res, next) {
     next(e);
   }
 });
-router.get("/:woeid", async function (req, res, next) {
-  try {
-    /**
-     * TODO:
-     * - for each twitter place
-     *    - find matching woeid in mongodb (to get its long/lat)
-     *    - set the log/lat on this object
-     *    - store in new collection (twitterPlaces || places)
-     *
-     * - for locations whose data I do not have... I need to get it. I
-     * will then configure the worker-cities service to fetch all
-     * "twitterPlaces" from mongo, feed them to the Yahoo API, and update
-     * those objects only (do NOT store them in the locations collection, where
-     * I keep the US locations data, which is NOT going to be used in
-     * production)
-     */
-    const uri = YahooAPI.getUriByWoeid(req.params.woeid);
-    const result = await YahooAPI.getForecast(uri);
-
-    res.json(result);
-  } catch (e) {
-    next(e);
-  }
-});
 
 module.exports = router;
