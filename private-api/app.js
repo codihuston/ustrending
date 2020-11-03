c3e0699b55a109328040e9103a9ebae20ffc5b0c
Codi Huston (Home) <codi.huston540@gmail.com>
2020-11-03 06:28:40 -0600

    remove(private-api): unused public files

--- private-api/app.js
+++ private-api/app.js
@@ -1,43 +1,42 @@
 /**
- * TODO: 
+ * TODO:
  * 1. [] connect to redis
  * 2. [] create route to fetch current trends (used by clients for initial
  * page load)
  * 3. [] implement socket.io / subscribe to redis / publish to clients
  *    on change? should this be a separate container/service?)
  */
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json(res.locals || { error: "An error has occured."});
+  res.json(res.locals || { error: "An error has occured." });
 });
 
 module.exports = app;
