de41c24238bac4d412d1254e960ed0f067bff0a2
Codi Huston (Home) <codi.huston540@gmail.com>
2020-11-03 06:26:30 -0600

    refactor(private-api): migrating db configs (wip)

--- private-api/db/index.js
+++ private-api/db/index.js
@@ -1,7 +1,9 @@
-
+// getting-started.js
 const Redis = require("ioredis");
+const mongoose = require("mongoose");
+const { Location } = require("../models/location");
 
-const client = new Redis({
+const cacheClient = new Redis({
   port: process.env.REDIS_PORT, // Redis port
   host: process.env.REDIS_HOST, // Redis host
   family: process.env.REDIS_FAMILY, // 4 (IPv4) or 6 (IPv6)
@@ -9,4 +11,61 @@ const client = new Redis({
   db: process.env.REDIS_DB,
 });
 
-module.exports.client = client;
+module.exports.cacheClient = cacheClient;
+
+mongoose.set("useFindAndModify", false);
+
+function getConnectionString() {
+  let credentialString = "";
+  const {
+    MONGO_HOST,
+    MONGO_PORT,
+    MONGO_DB,
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+  } = process.env;
+  if (MONGO_USERNAME && MONGO_PASSWORD) {
+    credentialString = `${MONGO_USERNAME}:${MONGO_PASSWORD}@`;
+  }
+
+  const str = `mongodb://${credentialString}${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
+  const safeStr = `mongodb://${
+    credentialString ? "REDACTED:" : ""
+  }${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
+
+  return [str, safeStr];
+}
+
+function dbConnect() {
+  return new Promise((resolve, reject) => {
+    try {
+      const [uri, safeUri] = getConnectionString();
+
+      console.log("MongoDB: Attempting to connect to: ", safeUri);
+
+      mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      const db = mongoose.connection;
+
+      db.on("error", function (error) {
+        reject(error);
+      });
+
+      db.on("connected", function () {
+        console.log("MongoDB: Connected to mongo!");
+        resolve(db);
+      });
+
+      db.on("disconnected", function () {
+        console.warn("MongoDB: Disconnected from mongo!");
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+module.exports.dbConnect = dbConnect;
