15864688a6795ad81058cbe157822d5aea9120c0
Codi Huston (Home) <codi.huston540@gmail.com>
2020-11-03 08:18:30 -0600

    feat(private-api): create HTTP interface for cache
    - this probably won't be needed, but I did it as a proof of concept
    - while some workers will cache keys based on an incremental value, this API is intended to be used to store cache data where keys aren't expected to change
    - the cache is intended to be used to write to places where keys aren't expected to be changed (keys that store final results)
    - there will be endpoints for these specifically

--- private-api/routes/index.js
+++ private-api/routes/index.js
@@ -8,7 +8,7 @@ const { initCache } = require("../db");
 router.get("/", async function (req, res, next) {
   try {
     const cacheClient = await initCache();
-    cacheClient.set("TEST", "TEST");
+    await cacheClient.set("TEST", "TEST");
     const locations = await Location.find();
 
     return res.json({ title: "Called Private API!", locations });
@@ -20,17 +20,25 @@ router.get("/", async function (req, res, next) {
 /**
  * fetch from the redis cache
  */
-router.get("/cache", function (req, res, next) {
-  debug("HIT!");
-  return res.json({ title: "Called Private API!" });
+router.get("/cache/:key", async function (req, res, next) {
+  try {
+    const cacheClient = await initCache();
+    return res.send(await cacheClient.get(req.params.key));
+  } catch (e) {
+    next(e);
+  }
 });
 
 /**
  * insert into the redis cache
  */
-router.post("/cache", function (req, res, next) {
-  debug("HIT!");
-  return res.json({ title: "Called Private API!" });
+router.post("/cache", async function (req, res, next) {
+  try {
+    const cacheClient = await initCache();
+    return res.send(await cacheClient.set(req.body.key, req.body.value));
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
