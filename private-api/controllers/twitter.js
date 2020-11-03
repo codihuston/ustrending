cad289c03ce8a138196f48d103132988fc0ff11c
Codi Huston (Home) <codi.huston540@gmail.com>
2020-11-03 15:51:54 -0600

    refactor(private-api): implement /twitter/places/update
    - will fetch locations from twitter and run them against yahoo using their woeid to fetch long/lat, timezone, and other data
    - then will persist in mongodb
    - will return all completed objects after they've been processed (will take a long time); may need to stream this instead

--- private-api/controllers/twitter.js
+++ private-api/controllers/twitter.js
@@ -1,8 +1,11 @@
 const debug = require("debug")("private-api:twitter-ctrl");
 
-const TwitterAPI = require("../api/twitter");
 const { initCache } = require("../db");
-const { secondsAs } = require("../lib/utils");
+const TwitterAPI = require("../api/twitter");
+const YahooAPI = require("../api/yahoo");
+// TODO: test error handling for places
+const { Place } = require("../models/place");
+const { secondsAs, sleep } = require("../lib/utils");
 
 module.exports.getPlaces = async function () {
   try {
@@ -24,6 +27,92 @@ module.exports.getPlaces = async function () {
       }
       return [];
     }
+  } catch (e) {
+    throw e;
+  }
+};
+
+module.exports.createPlaces = function (places) {
+  const results = [];
+
+  try {
+    const result = places.reduce((p, place) => {
+      return p.then(async () => {
+        console.log(`LOOP! ${new Date()}`, place.name);
+
+        const uri = YahooAPI.getUriByWoeid(place.woeid);
+        const {
+          name,
+          placeType,
+          url,
+          parentid,
+          country,
+          woeid,
+          countryCode,
+        } = place;
+
+        debug("Query Yahoo for Place: ", uri);
+
+        const response = await YahooAPI.getForecast(uri);
+        const { location } = response;
+
+        // verify that there is location data
+        if (response && location && Object.keys(location).length) {
+          const { region, timezone_id, lat, long } = location;
+
+          // build mongo queries
+          const query = {
+            woeid,
+          };
+          const update = {
+            name,
+            placeType,
+            url,
+            parentid,
+            country,
+            woeid,
+            countryCode,
+            // custom fields
+            region: region ? region.trim() : "",
+            timezone_id,
+            geo: {
+              type: "Point",
+              coordinates: [long, lat],
+            },
+            woeid,
+            yahooUri: uri,
+          };
+          const options = {
+            upsert: true,
+            new: true,
+            setDefaultsOnInsert: true,
+          };
+
+          console.log(query, update, options);
+
+          // upsert place in mongodb, return as plain JSON
+          const newInstance = await Place.findOneAndUpdate(
+            query,
+            update,
+            options
+          ).lean();
+
+          results.push(newInstance);
+        } // end if
+        else {
+          console.log(
+            "No response from YahooAPI for",
+            place.name,
+            place.country
+          );
+        }
+        return await sleep(1000);
+      });
+    }, Promise.resolve());
+
+    return result.then(() => {
+      return results;
+    });
   } catch (e) {
     throw e;
   }
--- private-api/routes/twitter.js
+++ private-api/routes/twitter.js
@@ -29,102 +29,31 @@ router.get("/places", async function (req, res, next) {
   } catch (e) {
     next(e);
   }
-  // try {
-  //   /**
-  //    * TODO:
-  //    * - for each twitter place
-  //    *    - find matching woeid in mongodb (to get its long/lat)
-  //    *    - set the log/lat on this object
-  //    *    - store in new collection (twitterPlaces || places)
-  //    *
-  //    * - for locations whose data I do not have... I need to get it. I
-  //    * will then configure the worker-cities service to fetch all
-  //    * "twitterPlaces" from mongo, feed them to the Yahoo API, and update
-  //    * those objects only (do NOT store them in the locations collection, where
-  //    * I keep the US locations data, which is NOT going to be used in
-  //    * production)
-  //    */
-  //   twitterPlaces.reduce((p, place) => {
-  //     return p.then(async () => {
-  //       console.log(`LOOP! ${new Date()}`, place.name);
-
-  //       const uri = YahooAPI.getUriByWoeid(place.woeid);
-  //       const {
-  //         name,
-  //         placeType,
-  //         url,
-  //         parentid,
-  //         country,
-  //         woeid,
-  //         countryCode,
-  //       } = place;
-
-  //       console.log("Sending response to: ", uri);
-
-  //       const result = await YahooAPI.getForecast(uri);
-  //       const { location } = result;
-
-  //       // verify that there is location data
-  //       if (result && location && Object.keys(location).length) {
-  //         const { region, timezone_id, lat, long } = location;
-
-  //         // build mongo queries
-  //         const query = {
-  //           woeid,
-  //         };
-  //         const update = {
-  //           name,
-  //           placeType,
-  //           url,
-  //           parentid,
-  //           country,
-  //           woeid,
-  //           countryCode,
-  //           // custom fields
-  //           region: region ? region.trim() : "",
-  //           timezone_id,
-  //           geo: {
-  //             type: "Point",
-  //             coordinates: [long, lat],
-  //           },
-  //           woeid,
-  //           yahooUri: uri,
-  //         };
-  //         const options = {
-  //           upsert: true,
-  //           new: true,
-  //           setDefaultsOnInsert: true,
-  //         };
-
-  //         console.log(query, update, options);
-
-  //         // upsert place in mongodb, return as plain JSON
-  //         const newInstance = await Place.findOneAndUpdate(
-  //           query,
-  //           update,
-  //           options
-  //         ).lean();
-  //       } // end if
-  //       else {
-  //         console.log(
-  //           "No response from YahooAPI for",
-  //           place.name,
-  //           place.country
-  //         );
-  //       }
-  //       return await sleep(1000);
-  //     });
-  //   }, Promise.resolve());
-
-  //   res.json({
-  //     message: "DONE!",
-  //   });
-  //   console.log("DONE");
-  // } catch (e) {
-  //   next(e);
-  // }
 });
 
+router.get("/places/update", async function (req, res, next) {
+  try {
+    /**
+     * TODO:
+     * - for each twitter place
+     *    - find matching woeid in mongodb (to get its long/lat)
+     *    - set the log/lat on this object
+     *    - store in new collection (twitterPlaces || places)
+     *
+     * - for locations whose data I do not have... I need to get it. I
+     * will then configure the worker-cities service to fetch all
+     * "twitterPlaces" from mongo, feed them to the Yahoo API, and update
+     * those objects only (do NOT store them in the locations collection, where
+     * I keep the US locations data, which is NOT going to be used in
+     * production)
+     */
+    const twitterPlaces = await TwitterController.getPlaces();
+    const places = await TwitterController.createPlaces(twitterPlaces);
+    res.json(places);
+  } catch (e) {
+    next(e);
+  }
+});
 router.get("/:woeid", async function (req, res, next) {
   try {
     /**
