59674b5b0940732f964c786c183d61f113c823fc
Codi Huston (Home) <codi.huston540@gmail.com>
2020-11-07 09:28:54 -0600

    refactor(private-api): up payload limit to 10mb
    - when google trends worker POSTs ~20 trends to the private api, a "413: payload too large" error occurs
    - fixed by upping the json body size limit

--- private-api/app.js
+++ private-api/app.js
@@ -20,8 +20,8 @@ var yahooRouter = require("./routes/yahoo");
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10mb", extended: false }));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
