0e51984d88140759a4723798df6d66cc941a8db8
codi huston <codi.huston540@gmail.com>
2021-02-13 23:06:31 -0600

    feat(client): memoize google trends region list

--- client/components/GoogleDailyTrendsByRegionList.tsx
+++ client/components/GoogleDailyTrendsByRegionList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { BsDash } from "react-icons/bs";
 import { ValueType } from "react-select";
 import { Box, Grid } from "@material-ui/core";
@@ -54,69 +54,83 @@ export function GoogleDailyTrendsByRegionList({
   withColor,
   withTitle,
 }: Props) {
-  let regions: GoogleRegionTrend[] = [];
+  return useMemo(() => {
+    let regions: GoogleRegionTrend[] = [];
 
-  if (!googleRegionTrends || !googleRegionTrends.length) {
-    return <span>Error: no google trends are provided!</span>;
-  }
+    if (!googleRegionTrends || !googleRegionTrends.length) {
+      return <span>Error: no google trends are provided!</span>;
+    }
 
-  // sort regions by name
-  if (isAlphabetical) {
-    regions = googleRegionTrends.filter((region) => {
-      const foundRegion = getSelectedRegionOption(region.name, selectedRegions);
-      return foundRegion ? foundRegion : false;
-    });
-  }
-  // sort regions by the position in which they were clicked
-  else {
-    // for each selected option
-    for (const option of selectedRegions) {
-      // pick it out of googleRegionTrends
-      for (const region of googleRegionTrends) {
-        if (option.value === region.name || option.label === region.name) {
-          regions.push(region);
+    // sort regions by name
+    if (isAlphabetical) {
+      regions = googleRegionTrends.filter((region) => {
+        const foundRegion = getSelectedRegionOption(
+          region.name,
+          selectedRegions
+        );
+        return foundRegion ? foundRegion : false;
+      });
+    }
+    // sort regions by the position in which they were clicked
+    else {
+      // for each selected option
+      for (const option of selectedRegions) {
+        // pick it out of googleRegionTrends
+        for (const region of googleRegionTrends) {
+          if (option.value === region.name || option.label === region.name) {
+            regions.push(region);
+          }
         }
       }
     }
-  }
 
-  if (!selectedRegions || selectedRegions.length <= 0) {
-    return <div>Please select a region.</div>;
-  } else if (!regions || regions.length <= 0) {
-    return <div>No regions found for {regions.join(", ")}.</div>;
-  }
+    if (!selectedRegions || selectedRegions.length <= 0) {
+      return <div>Please select a region.</div>;
+    } else if (!regions || regions.length <= 0) {
+      return <div>No regions found for {regions.join(", ")}.</div>;
+    }
 
-  return (
-    <Box>
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-        spacing={3}
-      >
-        {regions.map((region) => (
-          <Grid
-            item
-            key={region.name}
-            style={{
-              minWidth: "25%",
-            }}
-          >
-            <List>
-              <GoogleDailyTrendsByRegionListItem
-                colorMap={colorMap}
-                handleClick={handleClick}
-                region={region}
-                selectedRegions={selectedRegions}
-                sourceMap={sourceMap}
-                withColor={withColor}
-                withTitle={withTitle}
-              />
-            </List>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
-  );
+    return (
+      <Box>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={3}
+        >
+          {regions.map((region) => (
+            <Grid
+              item
+              key={region.name}
+              style={{
+                minWidth: "25%",
+              }}
+            >
+              <List>
+                <GoogleDailyTrendsByRegionListItem
+                  colorMap={colorMap}
+                  handleClick={handleClick}
+                  region={region}
+                  selectedRegions={selectedRegions}
+                  sourceMap={sourceMap}
+                  withColor={withColor}
+                  withTitle={withTitle}
+                />
+              </List>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    );
+  }, [
+    colorMap,
+    googleRegionTrends,
+    handleClick,
+    isAlphabetical,
+    selectedRegions,
+    sourceMap,
+    withColor,
+    withTitle,
+  ]);
 }
